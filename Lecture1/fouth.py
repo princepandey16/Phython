8c4b18154ab73134e95ee87f5414d10394efb92d
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-03-07 10:16:33 +0000

    for loop and boolean

---
+++ Lecture1/fouth.py
@@ -0,0 +1,17 @@
+# day = "monday"
+# tempreture = 30
+# rainning = True
+# notrainning = False
+
+# if day == "saturday" and tempreture > 26 and notrainning:
+#     print("go for swimming")
+# else:
+#     print("read book")
+
+# parrot = "blue"
+# for character in parrot:
+#     print(character)
+
+number = "9,3352.65521f,,mfdk"
+for character in number:
+    print(character)
